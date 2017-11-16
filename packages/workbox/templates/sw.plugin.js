a5ddf5992c61ee759a292d1af95a2cf0daac9d73
Pooya Parsa <pooya@pi0.ir>
2017-11-16 22:09:10 +0330

    feat(workbox): expose registration as window.$sw
    Also better error handeling when registration failed

--- packages/workbox/templates/sw.plugin.js
+++ packages/workbox/templates/sw.plugin.js
@@ -1,10 +1,14 @@
 window.onNuxtReady(() => {
   if (!('serviceWorker' in navigator)) {
-    console.warn('serviceWorker is not supported')
+    console.warn('Service workers are not supported.')
     return
   }
 
   navigator.serviceWorker.register('<%= options.swURL %>', {
     scope: '<%= options.swScope %>'
-  })
+  }).then(function(registration) {
+    window.$sw = registration
+  }).catch(function(error) {
+    console.error('Service worker registration failed:', error);
+  });
 })
