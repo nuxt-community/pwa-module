e7a9c008fc56e804fc83d47acfa9c46ab29b0e1f
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2018-04-02 12:18:41 -0700

    Rename back to original

--- packages/pwa/index.js
+++ packages/pwa/index.js
@@ -5,7 +5,7 @@ module.exports = function nuxtPWA (options) {
 
   return sequence(modules, module => {
     return this.requireModule({
-      src: `pwa-module/packages/${module}`,
+      src: `@nuxtjs/${module}`,
       options: options[module]
     })
   })
