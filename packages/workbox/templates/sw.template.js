be984d765dc006a0a9bb560913e101c79049afe6
Alexander Lichter <manniL@gmx.net>
2018-07-22 12:25:19 +0200

    chore: improve code layout

--- packages/workbox/templates/sw.template.js
+++ packages/workbox/templates/sw.template.js
@@ -3,11 +3,12 @@ importScripts(<%= options.importScripts.map(i => `'${i}'`).join(', ') %>)
 workbox.precaching.precacheAndRoute([], <%= JSON.stringify(options.wbOptions, null, 2) %>)
 
 <% if(options.clientsClaim) { %>
-  workbox.clientsClaim()
+workbox.clientsClaim()
 <% } %>
 
-<% options.runtimeCaching.forEach(r => {
+<%
+options.runtimeCaching.forEach(r => {
   const strategy = JSON.stringify(r.strategyOptions || {})
-  %>
+%>
 workbox.routing.registerRoute(new RegExp('<%= r.urlPattern %>'), workbox.strategies.<%= r.handler %>(<%= strategy %>), '<%= r.method %>')
 <% }) %>
