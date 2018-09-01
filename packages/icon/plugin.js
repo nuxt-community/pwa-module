95db50761cb4e4080711147ba64ec124d9b46433
Alexander Lichter <manniL@gmx.net>
2018-09-02 00:22:45 +0100

    remove console.log

--- packages/icon/plugin.js
+++ packages/icon/plugin.js
@@ -1,6 +1,5 @@
 export default async function (ctx, inject) {
   const moduleOptions = <%= serialize(options) %>
-    console.log("Icon object:", moduleOptions.icons)
   inject(moduleOptions.iconProperty.replace('$', ''), retrieveIcons(moduleOptions.icons))
 }
 
