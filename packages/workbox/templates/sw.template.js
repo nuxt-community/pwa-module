b20f13386cab8bfeedf9a8de488b2079393ab0b9
Pooya Parsa <pyapar@gmail.com>
2018-12-14 18:47:38 +0330

    chore: imrpove reabability of sw.template.js

--- packages/workbox/templates/sw.template.js
+++ packages/workbox/templates/sw.template.js
@@ -1,31 +1,64 @@
 importScripts(<%= options.importScripts.map((i) => `'${i}'`).join(', ') %>)
 
+// --------------------------------------------------
+// Configure
+// --------------------------------------------------
+
 <% if (options.config) {%>
+// Set workbox config
 workbox.setConfig(<%= JSON.stringify(options.config, null, 2) %>)
 <% } %>
 
+<% if (options.clientsClaim) { %>
+// Start controlling any existing clients as soon as it activates.
+workbox.clientsClaim()
+<% } %>
+
+<% if (options.skipWaiting) { %>
+// Skip over the SW waiting lifecycle stage
+workbox.skipWaiting()
+<% } %>
+
+// --------------------------------------------------
+// Precaches
+// --------------------------------------------------
+
+// Precache build artifacts
 workbox.precaching.precacheAndRoute([], <%= JSON.stringify(options.wbOptions, null, 2) %>)
+
 <% if (options.offlineAssets.length) { %>
+// Precache offlineAssets
 workbox.precaching.precacheAndRoute([<%= options.offlineAssets.map((i) => `'${i}'`).join(', ') %>])
 <% } %>
-<% if (options.offlinePage) { %>workbox.precaching.precacheAndRoute(['<%= options.offlinePage %>'])<% } %>
-<% if (options.cachingExtensions) { %><%= options.cachingExtensions %><% } %>
-<% if (options.clientsClaim) { %>workbox.clientsClaim()<% } %>
-<% if (options.skipWaiting) { %>workbox.skipWaiting()<% } %>
 
-<%
-options.runtimeCaching.forEach(r => {
-  const strategy = JSON.stringify(r.strategyOptions || {})
-%>
-workbox.routing.registerRoute(new RegExp('<%= r.urlPattern %>'), workbox.strategies.<%= r.handler %>(<%= strategy %>), '<%= r.method %>')
+<% if (options.offlinePage) { %>
+// Precache offlinePage
+workbox.precaching.precacheAndRoute(['<%= options.offlinePage %>'])
+<% } %>
+
+<% if (options.cachingExtensions) { %>
+// -- Start of cachingExtensions --
+<%= options.cachingExtensions %>
+// -- End of cachingExtensions --
+<% } %>
+
+// --------------------------------------------------
+// Runtime Caching
+// --------------------------------------------------
+
+// Register route handlers for runtimeCaching
+<% options.runtimeCaching.forEach(r => { %>workbox.routing.registerRoute(new RegExp('<%= r.urlPattern %>'), workbox.strategies.<%= r.handler %> (<%= JSON.stringify(r.strategyOptions || {}) %>), '<%= r.method %>')
 <% }) %>
 
 <% if (options.offlinePage) { %>
-// offlinePage support
-const strategy = workbox.strategies.networkOnly()
+// Register router handler for offlinePage
 workbox.routing.registerRoute(new RegExp('/.*'), ({event}) => {
-  return strategy.handle({event})
+  return workbox.strategies.networkOnly().handle({event})
     .catch(() => caches.match('<%= options.offlinePage %>'))
 })<% } %>
 
-<% if (options.routingExtensions) { %><%= options.routingExtensions %><% } %>
+<% if (options.routingExtensions) { %>
+// -- Start of routingExtensions --
+<%= options.routingExtensions %>
+// -- End of routingExtensions --
+<% } %>
