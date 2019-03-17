c35f6104a66ef125a0d33f8e32ee8f5f9a1cfc28
pooya parsa <pooya@pi0.ir>
2019-03-17 13:43:56 +0330

    feat(workbox): improve sw.register

--- packages/workbox/templates/sw.register.js
+++ packages/workbox/templates/sw.register.js
@@ -1,17 +1,22 @@
-import { Workbox } from 'workbox-cdn/workbox/workbox-window.<%= options.dev ? 'dev' : 'prod' %>.es5.mjs'
+export default async function (ctx, inject) {
+  let workbox = {}
 
-export default async function(ctx, inject) {
-  const workbox = new Workbox('<%= options.swURL %>', {
-    scope: '<%= options.swScope %>'
-  })
+  try {
+    if (!'serviceWorker' in navigator) {
+      throw new Error('Serviceworker is not supported!')
+    }
+
+    const { Workbox } = require('workbox-cdn/workbox/workbox-window.<%= options.dev ? 'dev' : 'prod' %>.es5.mjs')
+
+    workbox = new Workbox('<%= options.swURL %>', {
+      scope: '<%= options.swScope %>'
+    })
+
+    workbox.register()
+  } catch (e) {
+    console.warn('Cannot register workbox:', e)
+  }
 
   // Inject as $workbox
   inject('workbox', workbox)
-
-  if (!'serviceWorker' in navigator) {
-    workbox._unsupported = true
-    return // Unsupported browser!
-  }
-
-  workbox.register()
 }
