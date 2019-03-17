9aa76f89bdf34cefc018ae602a2514bf9ea38944
pooya parsa <pooya@pi0.ir>
2019-03-17 22:36:16 +0330

    feat: improve sw.register error handling

--- packages/workbox/templates/sw.register.js
+++ packages/workbox/templates/sw.register.js
@@ -1,20 +1,25 @@
-export default async function (ctx, inject) {
+function onError(error) {<% if (options.dev) { %>console.error('Error registering workbox:', error) <% } %>}
+
+export default function (ctx, inject) {
   let workbox = {}
 
   try {
+    // workbox-window does not detects unsupported browsers
     if (!'serviceWorker' in navigator) {
-      throw new Error('Serviceworker is not supported!')
+      throw new Error('serviceWorker is not supported in current browser!')
     }
 
+    // Use require() instead of import() to prevent creating extra chunk
+    // Use es5 version to prevent crashing older browsers while parsing bundle
     const { Workbox } = require('workbox-cdn/workbox/workbox-window.<%= options.dev ? 'dev' : 'prod' %>.es5.mjs')
 
     workbox = new Workbox('<%= options.swURL %>', {
       scope: '<%= options.swScope %>'
     })
 
-    workbox.register()
-  } catch (e) {
-    console.warn('Cannot register workbox:', e)
+    workbox.register().catch(onError)
+  } catch (error) {
+    onError(error)
   }
 
   // Inject as $workbox
