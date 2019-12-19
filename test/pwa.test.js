b0092da936f6280e54a5270c6f1750cf1d163137
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-12-19 12:03:59 -0300

    test: use `@nuxtjs/module-test-utils`

--- test/pwa.test.js
+++ test/pwa.test.js
@@ -1,25 +1,16 @@
 const path = require('path')
+const { generate, loadConfig } = require('@nuxtjs/module-test-utils')
 const klawSync = require('klaw-sync')
 const fs = require('fs-extra')
-const { Nuxt, Builder } = require('nuxt-edge')
 
 const getRelativePath = fileObj => path.relative(__dirname, fileObj.path)
-
 const noJS = item => !/\.js/.test(item)
 
 describe('pwa', () => {
   let nuxt
 
-  test('build project', async () => {
-    nuxt = new Nuxt(require('./fixture/nuxt.config'))
-    await nuxt.ready()
-
-    // Generate before running tests as a known issue
-    // Build for more coverage
-    const builder = new Builder(nuxt)
-    await builder.build()
-    // const generator = new Generator(nuxt)
-    // await generator.generate({ build: false })
+  beforeAll(async () => {
+    ({ nuxt } = await generate(loadConfig(__dirname)))
   }, 60000)
 
   test('build files (.nuxt)', () => {
