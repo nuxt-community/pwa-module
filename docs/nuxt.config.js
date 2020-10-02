5f7fe23fd0081a131181a627b336acc45902e7a3
Dennis Herzberg <dennishzg@gmail.com>
2020-10-02 18:22:33 +0200

    docs: add pwa manifest name

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -7,5 +7,10 @@ export default theme({
     server: 'https://ackee.nuxtjs.com',
     domainId: 'a2998bc2-56dd-47fa-9d94-9781411bd1f9',
     detailed: true
+  },
+  pwa: {
+    manifest: {
+      name: 'Nuxt PWA'
+    }
   }
 })
