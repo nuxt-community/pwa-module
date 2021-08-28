f70c387bf6035e7693c2d3d78222de184c921cc8
Milad Hosseinpour <m.hosseinpour@rayan.parsi>
2021-08-28 11:57:34 +0430

    add auto update to change the app content

--- templates/workbox/workbox.js
+++ templates/workbox/workbox.js
@@ -16,3 +16,15 @@ async function register() {
 
 window.$workbox = register()
   .catch(error => {<% if (options.dev) { %> console.error('Error registering workbox:', error) <% } %>})
+
+// Auto update application when something changes and update is available!
+const workbox = await window.$workbox
+if (workbox) {
+  workbox.addEventListener('installed', event => {
+    if (event.isUpdate) {
+      console.debug('There is an update for the PWA, reloading...')
+      window.location.reload()
+    }
+  })
+}
+
